f32c3cc24a660a3c7723c0bba29abbecb4b3535d
skylabel <40485759+skylabel@users.noreply.github.com>
2018-07-12 15:22:11 +0200

    refactoring
    refactoring

--- MultiArmBandit/src/main/Main.java
+++ MultiArmBandit/src/main/Main.java
@@ -9,19 +9,21 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException, PlayerIsNotPresentException, PlayerIsAlreadyPresentException, PlayerDataCorruptionException, LoginRequiredException {
+        Player uniformPlayer = createLuca();
+        IArchiveDB archive = new ArchiveDB();
+
+        Application application = new Application(archive);
+        application.registerUser(uniformPlayer);
+        application.login("Topolino");
+        application.play();
+    }
+
+	private static Player createLuca() {
         ExplorationRate rate = new ExplorationRate(50);
         String birthdate = new String("1984-06-19");
         String username=new String("Topolino");
-        Player uniformPlayer = new UniformExplorationPlayer(username,"Luca", birthdate, rate);
-        IArchiveDB archive = new ArchiveDB();
-
-
-        Application application = new Application(archive);
-      //  application.registerUser(uniformPlayer);
-
-        application.login(username);
-
-        application.play();
-    }
+		Player uniformPlayer = new UniformExplorationPlayer(username,"Luca", birthdate, rate);
+		return uniformPlayer;
+	}
 
 }
